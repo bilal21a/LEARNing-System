348fc8899a14b57f8515ac5ead2f6745fa7f68a4
Bilal Anwar <b.anwar21.a@gmail.com>
2021-12-08 12:11:54 +0500

    mig

---
+++ database/migrations/2021_12_08_052733_create_schedules_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSchedulesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schedules', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('teacher_id')->constrained('users');
+            $table->binary('mon');
+            $table->binary('tue');
+            $table->binary('wed');
+            $table->binary('thu');
+            $table->binary('fri');
+            $table->binary('sat');
+            $table->binary('sun');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('schedules');
+    }
+}
