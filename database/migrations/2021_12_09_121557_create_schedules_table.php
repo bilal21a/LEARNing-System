adb483eaa526c33451ad4bebe364775cf487785a
Bilal <b.anwar21.a@gmail.com>
2021-12-10 00:08:36 +0500

    timespan

--- database/migrations/2021_12_09_121557_create_schedules_table.php
+++ database/migrations/2021_12_09_121557_create_schedules_table.php
@@ -16,8 +16,35 @@ class CreateSchedulesTable extends Migration
         Schema::create('schedules', function (Blueprint $table) {
             $table->id();
             $table->foreignId('teacher_id')->constrained('users');
-            $table->foreignId('time_spans_id')->constrained('time_spans');
             $table->string('level')->nullable();
+            $table->integer('mon_start')->default(100)->nullable();
+            $table->string('mon_start_val')->default('no time')->nullable();
+            $table->integer('mon_end')->default(100)->nullable();
+            $table->string('mon_end_val')->default('no time')->nullable();
+            $table->integer('tue_start')->default(100)->nullable();
+            $table->string('tue_start_val')->default('no time')->nullable();
+            $table->integer('tue_end')->default(100)->nullable();
+            $table->string('tue_end_val')->default('no time')->nullable();
+            $table->integer('wed_start')->default(100)->nullable();
+            $table->string('wed_start_val')->default('no time')->nullable();
+            $table->integer('wed_end')->default(100)->nullable();
+            $table->string('wed_end_val')->default('no time')->nullable();
+            $table->integer('thu_start')->default(100)->nullable();
+            $table->string('thu_start_val')->default('no time')->nullable();
+            $table->integer('thu_end')->default(100)->nullable();
+            $table->string('thu_end_val')->default('no time')->nullable();
+            $table->integer('fri_start')->default(100)->nullable();
+            $table->string('fri_start_val')->default('no time')->nullable();
+            $table->integer('fri_end')->default(100)->nullable();
+            $table->string('fri_end_val')->default('no time')->nullable();
+            $table->integer('sat_start')->default(100)->nullable();
+            $table->string('sat_start_val')->default('no time')->nullable();
+            $table->integer('sat_end')->default(100)->nullable();
+            $table->string('sat_end_val')->default('no time')->nullable();
+            $table->integer('sun_start')->default(100)->nullable();
+            $table->string('sun_start_val')->default('no time')->nullable();
+            $table->integer('sun_end')->default(100)->nullable();
+            $table->string('sun_end_val')->default('no time')->nullable();
             $table->timestamps();
         });
     }
