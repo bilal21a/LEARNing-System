2f2f7a38c97bae16f4234684f68add29bf179c99
Bilal Anwar <b.anwar21.a@gmail.com>
2021-12-10 14:51:33 +0500

    tabs

--- resources/views/frontend/teacher/dashboard-classrooms.blade.php
+++ resources/views/frontend/teacher/dashboard-classrooms.blade.php
@@ -4,7 +4,35 @@
 @endsection
 
 @section('inline_css')
-<style></style>
+<style>
+		ul.tabs{
+			margin: 0px;
+			padding: 0px;
+			list-style: none;
+		}
+		ul.tabs li{
+			background: none;
+			color: #222;
+			display: inline-block;
+			padding: 10px 15px;
+			cursor: pointer;
+		}
+
+		ul.tabs li.current{
+			background: #ededed;
+			color: #222;
+		}
+
+		.tab-content{
+			display: none;
+			background: #ededed;
+			padding: 15px;
+		}
+
+		.tab-content.current{
+			display: inherit;
+		}
+</style>
 @endsection
 
 @section('content')
@@ -101,6 +129,43 @@
                                     <form class="form-3" action="" method="">
                                         <div class="row">
                                             <div class="col-12">
+
+                                                <div class="container">
+
+                                                    <ul class="tabs">
+                                                        <li class="tab-link current" data-tab="tab-1">Tab One</li>
+                                                        <li class="tab-link" data-tab="tab-2">Tab Two</li>
+                                                        <li class="tab-link" data-tab="tab-3">Tab Three</li>
+                                                        <li class="tab-link" data-tab="tab-4">Tab Four</li>
+                                                        <li class="tab-link" data-tab="tab-5">Tab 5</li>
+                                                        <li class="tab-link" data-tab="tab-6">Tab 6</li>
+                                                        <li class="tab-link" data-tab="tab-7">Tab 7</li>
+                                                    </ul>
+
+                                                    <div id="tab-1" class="tab-content current">
+                                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                                                    </div>
+                                                    <div id="tab-2" class="tab-content">
+                                                         Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                                                    </div>
+                                                    <div id="tab-3" class="tab-content">
+                                                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                                                    </div>
+                                                    <div id="tab-4" class="tab-content">
+                                                        Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                                                    </div>
+                                                    <div id="tab-5" class="tab-content">
+                                                        Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                                                    </div>
+                                                    <div id="tab-6" class="tab-content">
+                                                        Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                                                    </div>
+                                                    <div id="tab-7" class="tab-content">
+                                                        Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                                                    </div>
+
+                                                </div><!-- container -->
+
                                                 <div id="form-details-of-classrooms">
                                                     <ul>
                                                         <li>
@@ -342,8 +407,22 @@
 @section('js_after')
 <script>
 
+$(document).ready(function(){
 
-    $( document ).ready(function() {
+	$('ul.tabs li').click(function(){
+		var tab_id = $(this).attr('data-tab');
+
+		$('ul.tabs li').removeClass('current');
+		$('.tab-content').removeClass('current');
+
+		$(this).addClass('current');
+		$("#"+tab_id).addClass('current');
+	})
+
+})
+
+
+
         $(".hour_time").change(function(){
             var field = $(this).prev('input').val();
             var time = $( this ).val();
@@ -363,7 +442,6 @@
             }
             });
 
-    });
     });
 
 </script>
