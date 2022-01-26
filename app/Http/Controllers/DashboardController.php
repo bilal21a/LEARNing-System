2da5ec774b28dfd02c5f99dbe695185c4a0d7b69
Bilal Anwar <b.anwar21.a@gmail.com>
2022-01-26 19:03:33 +0500

    free trail

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -16,45 +16,44 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        $teacher_id= Auth::user()->id;
-        $time_all_day = HrsTime::select('name','value')->get()->ToArray();
-        $schedule =  Schedule::where('teacher_id' , $teacher_id)->get();
+        $teacher_id = Auth::user()->id;
+        $time_all_day = HrsTime::select('name', 'value')->get()->ToArray();
+        $schedule =  Schedule::where('teacher_id', $teacher_id)->get();
         if ($schedule->count()) {
             // exists
-            $schedule_first=$schedule->where('level',1)->first();
-            $schedule_second=$schedule->where('level',2)->first();
-            $schedule_third=$schedule->where('level',3)->first();
-            $schedule_fourth=$schedule->where('level',4)->first();
-        }else{
-            $schedule_first='';
-            $schedule_second='';
-            $schedule_third='';
-            $schedule_fourth='';
-
+            $schedule_first = $schedule->where('level', 1)->first();
+            $schedule_second = $schedule->where('level', 2)->first();
+            $schedule_third = $schedule->where('level', 3)->first();
+            $schedule_fourth = $schedule->where('level', 4)->first();
+        } else {
+            $schedule_first = '';
+            $schedule_second = '';
+            $schedule_third = '';
+            $schedule_fourth = '';
         }
-        return view('frontend.teacher.dashboard-classrooms',compact('time_all_day','schedule','schedule_first','schedule_second','schedule_third','schedule_fourth'));
+        return view('frontend.teacher.dashboard-classrooms', compact('time_all_day', 'schedule', 'schedule_first', 'schedule_second', 'schedule_third', 'schedule_fourth'));
     }
-    public function schedule_save_second($field,$time,$time_data)
+    public function schedule_save_second($field, $time, $time_data)
     {
-        $field = explode( '-', $field );
-        $level=$field[1];
+        $field = explode('-', $field);
+        $level = $field[1];
         try {
-            $teacher_id= Auth::user()->id;
-            $schedule1 =  Schedule::where('teacher_id' , $teacher_id)->where('level' , $level)->first();
-            if ($schedule1==null) {
+            $teacher_id = Auth::user()->id;
+            $schedule1 =  Schedule::where('teacher_id', $teacher_id)->where('level', $level)->first();
+            if ($schedule1 == null) {
                 $schedule = new Schedule();
-                $schedule->teacher_id=$teacher_id;
-                $schedule->level=$level;
-            }else{
-                $schedule=$schedule1;
+                $schedule->teacher_id = $teacher_id;
+                $schedule->level = $level;
+            } else {
+                $schedule = $schedule1;
             }
 
-            $parm=$field[0];
-            $parm_val=$field[0]."_val";
-            $schedule->$parm=$time;
-            $schedule->$parm_val=$time_data;
+            $parm = $field[0];
+            $parm_val = $field[0] . "_val";
+            $schedule->$parm = $time;
+            $schedule->$parm_val = $time_data;
             $schedule->save();
-            return $field[0]." value has been updated";
+            return $field[0] . " value has been updated";
         } catch (\Throwable $th) {
             return $th->getMessage();
         }
@@ -62,12 +61,12 @@ class DashboardController extends Controller
 
     public function free_trail()
     {
-        $free_trail = DB::table('booked_free_trails as bft')->where('teacher_id',auth()->user()->id)
-        ->join('users as u', 'u.id', '=', 'bft.student_id')
-        ->select('u.name as StdName','bft.*')
-        ->get();
+        $free_trails = DB::table('booked_free_trails as bft')->where('teacher_id', auth()->user()->id)
+            ->join('users as u', 'u.id', '=', 'bft.student_id')
+            ->select('u.name as StdName', 'bft.*')
+            ->get();
 
-        dd($free_trail);
-        return view('frontend.teacher.free_trail');
+        // dd($free_trail);
+        return view('frontend.teacher.free_trail', compact('free_trails'));
     }
 }
